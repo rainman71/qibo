2b62b36980bef5d0cdfc941666e5515009db573f
Reynold Yordy <ryordy@gmail.com>
2025-09-24 17:39:33 -0500

    Update RestartTourButton.jsx

--- components/RestartTourButton.jsx
+++ components/RestartTourButton.jsx
@@ -4,9 +4,9 @@
 export default function RestartTourButton() {
   return (
     <button
-      onClick={() => window.restartTour?.()}
-      className="ml-3 px-3 py-2 rounded border"
       type="button"
+      onClick={() => window.restartTour?.()}   // <- removed stray dot
+      className="ml-3 px-3 py-2 rounded border"
     >
       Restart Tour
     </button>
